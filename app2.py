633a18b0cd8543962ad356be800e69b113f2c791
Jonatan Mora <jonatan.mora@tsa.com.mx>
2025-01-13 12:19:15 -0600

    Modificaciones a estructura de codigo

--- app2.py
+++ app2.py
@@ -18,22 +18,37 @@ async def read_root():
     return {"message": "Welcome Security path"}
 
 @secure_router.post("/security/cbac/validate")
-async def cbac_validate(request: RequestCbacModel, x_api_key: Optional[str] = Header(None)):
+async def cbac_validate(cbacModel: RequestCbacModel, x_api_key: Optional[str] = Header(None)):
     # Validar el header 'api-key'
     if not x_api_key or x_api_key is None:
         raise HTTPException(status_code=403, detail="Invalid API Key")
 
     # Respuesta en caso de éxito
     print(x_api_key)
-    print(request.json())
-    return {
-        "data": {
-            "status": "SUCCESS",
-            "transactionId": "12345"
+    print(cbacModel.token)
+    print(cbacModel.username)
+    print(cbacModel.seed)
+    if(cbacModel.token == "211111"):
+        return {
+            "data": {
+                "status": "INVALID",
+                "transactionId": cbacModel.transactionId
+            }
         }
-    }
+    else:
+        return {
+            "data": {
+                "status": "SUCCESS",
+                "transactionId": cbacModel.transactionId
+            }
+        }
 
 if __name__ == "__main__":
-    config = uvicorn.Config("app2:secure_router", port=8443,host="0.0.0.0", ssl_certfile="certs/server.crt",ssl_keyfile="certs/server.key",ssl_ca_certs="certs/ca.crt", log_level="info")
+    config = uvicorn.Config("app2:secure_router",
+                            port=8443,host="0.0.0.0",
+                            ssl_certfile="certs/server.crt",
+                            ssl_keyfile="certs/server.key",
+                            ssl_ca_certs="certs/ca.crt",
+                            log_level="info")
     server = uvicorn.Server(config)
     server.run()
